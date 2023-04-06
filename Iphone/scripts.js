8c457bb954cfd8ec69d8035339c38474c2a7aef3
GabrielB <gabriel.belo.dm@gmail.com>
2023-04-06 20:49:20 -0300

    commit inicial

---
+++ Iphone/scripts.js
@@ -0,0 +1,14 @@
+
+function trocaCor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+
+        
+}
+
+function trocaImg(imagem) {
+    let iPhone = document.querySelector(".iPhone")
+    iPhone.src = imagem
+
+
+}
